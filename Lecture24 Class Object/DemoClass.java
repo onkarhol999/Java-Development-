2666649fb5ae113a072346082df916a37a253445
Onkar hol <holonkar2003@gmail.com>
2024-10-01 05:18:02 +0530

    Super method was added

--- Lecture24 Class Object/DemoClass.java
+++ Lecture24 Class Object/DemoClass.java
@@ -1,7 +1,16 @@
-public class DemoClass {
+class claculator {
+    public int add(int num1, int num2) {
+        int result = num1 + num2;
+        return result;
+    }
+}
+
+class DemoClass {
     public static void main(String[] args) {
         int num1 = 4;
         int num2 = 5;
-        System.out.println(num1 + num2);
+        claculator clac = new claculator();
+        int result = clac.add(num1, num2);
+        System.err.println(result);
     }
 }
---
+++ Lecture53 Super Method/SuperMethod.java
@@ -0,0 +1,28 @@
+class A {
+    public A() {
+        System.out.println("in A");
+    }
+
+    public A(int n) {
+        System.out.println("in parameterized A");
+    }
+}
+
+class B extends A {
+    public B() {
+        super();
+        System.out.println("in B");
+    }
+
+    public B(int n) {
+        super(n);
+        System.out.println("in parametarised B");
+    }
+}
+
+public class SuperMethod {
+    public static void main(String[] args) {
+        B obj = new B(8);
+        // System.out.println("Onkar");
+    }
+}
